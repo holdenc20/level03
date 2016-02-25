fe839245eb282765fbceecb5c48a9bd272db5c10
Shiva Malhotra <Shiva.malhotra.email@gmail.com>
2016-02-24 18:27:46 -0800

    z

---
+++ src/level03/Code4Life.java
@@ -0,0 +1,39 @@
+package level03;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class Code4Life {
+	public static void main(String[] args) {
+	/*
+	 * Ask the user how many hours they spent coding this week.
+	 * 
+	 * 1. If it's more than 3, tell them they're a Code Ninja.
+	 * 
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+	 * 
+	 * 3. If it's more than 5, play the Batman theme song.
+	 */
+	String x=JOptionPane.showInputDialog(null,"How many hour have you coded this week?");
+	int y=Integer.parseInt(x);
+	if (y>3){
+		
+	}
+		playBatmanTheme();
+	}
+
+
+	private static void playBatmanTheme() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/batman.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(60002);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}}
